36f1081f4b7eef26807210f832cc6c1d6553e038
DhruvMatariya <dhruvmatariya1@gmail.com>
2026-07-09 20:57:02 +0530

    [3532] [Time Beats: 83.84%] [Memory Beats: 66.03%] - LeetPush

--- DCP-07-26/3532-Path-Existence-Queries-in-a-Graph-I.cpp
+++ DCP-07-26/3532-Path-Existence-Queries-in-a-Graph-I.cpp
@@ -1,48 +1,20 @@
-class DisjointSet{
-vector<int>parent,size;
-public:
-DisjointSet(int n){
-    parent.resize(n+1,0);
-    size.resize(n+1,1);
-    for(int i=0;i<=n;i++) parent[i]=i;
-}
-int findUpar(int node){
-    if(parent[node]==node) return node;
-    return parent[node]=findUpar(parent[node]);
-}
-void unionBySize(int u,int v){
-    int ulp_u=findUpar(u);
-    int ulp_v=findUpar(v);
-    if(ulp_u==ulp_v) return;
-    if(size[ulp_u]<size[ulp_v]){
-        parent[ulp_u]=ulp_v;
-        size[ulp_v]+=size[ulp_u];
-    }
-    else{
-         parent[ulp_v]=ulp_u;
-        size[ulp_u]+=size[ulp_v];
-    }
-}
-};
 class Solution {
 public:
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        DisjointSet ds(n);
-        for(int i=1;i<n;i++){
-           if(nums[i]-nums[i-1]<=maxDiff){
-            int u=i;
-            int v=i-1;
-            if(ds.findUpar(u)!=ds.findUpar(v)){
-                ds.unionBySize(u,v);
+          vector<int> component(n, 0);
+        int compNo = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i] - nums[i - 1] > maxDiff){
+                compNo++;
             }
-           }
+            component[i] = compNo;
         }
-        vector<bool>ans;
-        for(auto it: queries){
-            int u=it[0];
-            int v=it[1];
-            ans.push_back(ds.findUpar(u)==ds.findUpar(v));
+        vector<bool> sol;
+
+        for(auto &it : queries){
+            sol.push_back(component[it[0]] == component[it[1]]);
         }
-        return ans;
+
+        return sol;
     }
 };
